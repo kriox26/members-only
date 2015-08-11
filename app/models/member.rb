369745b951804d99828f2f600912a72c0d9a64e4
kriox26 <matias.pan26@gmail.com>
2015-08-10 23:35:05 -0300

    add authenticated method

--- app/models/member.rb
+++ app/models/member.rb
@@ -30,6 +30,12 @@ class Member < ActiveRecord::Base
 	update_attribute(:remember_digest, Member.digest(remember_token))
   end
 
+  # Returns true if the given token matches the digest
+  def authenticated?(remember_token)
+	return false if remember_digest.nil?
+	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
   def forget
 	update_attribute(:remember_digest, nil)
   end
